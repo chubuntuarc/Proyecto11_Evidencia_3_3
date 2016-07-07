f48d27f25628d9c5e9a6e4be184350724c521c6f
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-07-06 18:31:04 -0600

    Declaración de variables y clase para el calculo de divisas

---
+++ app/src/main/java/com/example/arciniega/proyecto11_evidencia_3_3/Calculo.java
@@ -0,0 +1,48 @@
+package com.example.arciniega.proyecto11_evidencia_3_3;
+
+/**
+ * Created by arciniega on 06/07/16.
+ */
+public class Calculo {
+    private double val1;
+    private double val2;
+    private double res;
+    private String seleccion;
+
+    public double getVal1() {
+        return val1;
+    }
+
+    public void setVal1(double val1) {
+        this.val1 = val1;
+    }
+
+    public double getVal2() {
+        return val2;
+    }
+
+    public void setVal2(double val2) {
+        this.val2 = val2;
+    }
+
+    public double getRes() {
+        return res;
+    }
+
+    public void setRes(double res) {
+        this.res = res;
+    }
+
+    public String getSeleccion() {
+        return seleccion;
+    }
+
+    public void setSeleccion(String seleccion) {
+        this.seleccion = seleccion;
+    }
+
+    public Calculo() {
+
+    }
+
+}
--- app/src/main/java/com/example/arciniega/proyecto11_evidencia_3_3/MainActivity.java
+++ app/src/main/java/com/example/arciniega/proyecto11_evidencia_3_3/MainActivity.java
@@ -2,8 +2,25 @@ package com.example.arciniega.proyecto11_evidencia_3_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Declaración de las variables
+    private Spinner spinner;
+    private TextView divisa1;
+    private TextView divisa2;
+    private TextView resultado;
+    private EditText editDivisa1;
+    private Button btnConvertir;
+
+    // Variables para el calculo
+    private String dato;
+    private double val1;
+    private double val2;
+    private double res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
